6c4fcf8fee8aad18aa008cbbe3fd880e633881d0
Jan Przybysz <janprzybysz2@gmail.com>
2019-06-19 12:26:56 +0200

    Updates

---
+++ precache-manifest.8fdcd141b060e71bb7db35cccd95ee02.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ad8e0ffc3900e87cb5cf5840906a3e8",
+    "url": "/github-finder/index.html"
+  },
+  {
+    "revision": "335173d01e89fd6b30f2",
+    "url": "/github-finder/static/css/main.f65227a3.chunk.css"
+  },
+  {
+    "revision": "ddd346e0af0970134a27",
+    "url": "/github-finder/static/js/2.e56676bb.chunk.js"
+  },
+  {
+    "revision": "335173d01e89fd6b30f2",
+    "url": "/github-finder/static/js/main.d6af272c.chunk.js"
+  },
+  {
+    "revision": "a06ceeb70725f86afdce",
+    "url": "/github-finder/static/js/runtime~main.cb74c248.js"
+  }
+]);
